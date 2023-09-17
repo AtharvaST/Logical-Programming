e3eb3970349d8411981396ad70d59593507d1928
AtharvaST <teliatharva@gmail.com>
2023-09-17 23:53:29 +0530

    Logical Programming

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -3,28 +3,27 @@ package com.jsp.Numberbased;
 import java.util.Scanner;
 
 public class PerfectNumber {
-	
-	public static boolean isperfect(int n){
-		int sum=0;
-		//to find factors
-		for(int i=1;i<n;i++) {
-			if(n%i==0) {
-				sum=sum+i;
+
+	public static boolean isperfect(int n) {
+		int sum = 0;
+		// to find factors
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) {
+				sum = sum + i;
 			}
 		}
-		if(sum==n) {
+		if (sum == n) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
+
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a number");
-		int n=sc.nextInt();
+		int n = sc.nextInt();
 		System.out.println(isperfect(n));
-		
+
 	}
 }
-
-
